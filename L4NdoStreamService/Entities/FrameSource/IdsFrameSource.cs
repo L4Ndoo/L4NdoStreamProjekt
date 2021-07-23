2809520a7d112b2c7778a702828a53efe97ff91b
Thomas Frischholz <Thomas.Frischholz@igz.com>
2021-07-23 12:27:15 +0200

    Changed scaling of IdsFramesource to use camera to scale (faster) TODO: Implement Threadsafety

--- L4NdoStreamService/Entities/FrameSource/IdsFrameSource.cs
+++ L4NdoStreamService/Entities/FrameSource/IdsFrameSource.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,16 +10,79 @@ namespace L4NdoStreamService.Entities.FrameSource
 {
     public class IdsFrameSource : IFrameSource
     {
-        public float Scale { get; set; } = 1f;
+        public float Scale
+        {
+            get => this._scale;
+            set
+            {
+                int nearest = 0;
+                float nearestValue = 1;
+                for(int i = 2; i < 9; i+=2)
+                {
+                    float val = Math.Abs((i * value) - 1);
+                    if(val < nearestValue)
+                    {
+                        nearestValue = val;
+                        nearest = i;
+                    }
+                }
+                for(int i = 1; i < 17; i+=15)
+                {
+                    float val = Math.Abs((i * value) - 1);
+                    if (val < nearestValue)
+                    {
+                        nearestValue = val;
+                        nearest = i;
+                    }
+                }
+
+                this._camera.Acquisition.Stop();
+                this._camera.Memory.GetList(out int[] idList);
+                this._camera.Memory.Free(idList);
+                switch (nearest)
+                {
+                    case 1:
+                        this._camera.Size.Subsampling.Set(uEye.Defines.SubsamplingMode.Disable);
+                        this._camera.Size.AOI.Set(0, 0, 2448, 2048);
+                        break;
+                    case 2:
+                        this._camera.Size.Subsampling.Set(uEye.Defines.SubsamplingMode.Horizontal2X | uEye.Defines.SubsamplingMode.Vertical2X);
+                        this._camera.Size.AOI.Set(0, 0, 1224, 1024);
+                        break;
+                    case 4:
+                        this._camera.Size.Subsampling.Set(uEye.Defines.SubsamplingMode.Horizontal4X | uEye.Defines.SubsamplingMode.Vertical4X);
+                        this._camera.Size.AOI.Set(0, 0, 612, 512);
+                        break;
+                    case 6:
+                        this._camera.Size.Subsampling.Set(uEye.Defines.SubsamplingMode.Horizontal6X | uEye.Defines.SubsamplingMode.Vertical6X);
+                        this._camera.Size.AOI.Set(0, 0, 408, 340);
+                        break;
+                    case 8:
+                        this._camera.Size.Subsampling.Set(uEye.Defines.SubsamplingMode.Horizontal8X | uEye.Defines.SubsamplingMode.Vertical8X);
+                        this._camera.Size.AOI.Set(0, 0, 304, 254);
+                        break;
+                    case 16:
+                        this._camera.Size.Subsampling.Set(uEye.Defines.SubsamplingMode.Horizontal16X | uEye.Defines.SubsamplingMode.Vertical16X);
+                        this._camera.Size.AOI.Set(0, 0, 152, 126);
+                        break;
+                }
+                this._camera.Memory.Allocate();
+                this._camera.Memory.GetList(out idList);
+                this._camera.Memory.Sequence.Add(idList);
+                this._camera.Acquisition.Capture();
+                this._scale = 1f / nearest;
+            }
+        }
 
         private readonly Camera _camera;
         private Bitmap _lastGrabbed;
+        private float _scale;
 
         public IdsFrameSource()
         {
             uEye.Info.Camera.GetCameraList(out CameraInformation[] camInfos);
-            this._camera = new Camera(camInfos.First().CameraID);
-            for (int i = 0; i < 3; i++) { this._camera.Memory.Allocate(); }
+           this._camera = new Camera(camInfos.First().CameraID);
+            this._camera.Memory.Allocate();
             this._camera.Memory.GetList(out int[] idList);
             this._camera.Memory.Sequence.Add(idList);
             this._camera.Acquisition.Capture();
@@ -32,12 +96,14 @@ namespace L4NdoStreamService.Entities.FrameSource
             this._camera.Memory.GetLast(out int memoryId);
             this._camera.Memory.CopyToBitmap(memoryId, out Bitmap bitmap);
 
-            using Bitmap converted = new (bitmap.Width, bitmap.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            if(bitmap == null) { return null; }
+
+            Bitmap converted = new (bitmap.Width, bitmap.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             using Graphics gr = Graphics.FromImage(converted);
             gr.DrawImage(bitmap, new Rectangle(0, 0, converted.Width, converted.Height));
 
             this._lastGrabbed?.Dispose();
-            this._lastGrabbed = new Bitmap(converted, (int)(converted.Width * this.Scale), (int)(converted.Height * this.Scale));
+            this._lastGrabbed = converted;
             BitmapData data = this._lastGrabbed.LockBits(
                     new Rectangle(0, 0, this._lastGrabbed.Width, this._lastGrabbed.Height),
                     ImageLockMode.ReadOnly,
