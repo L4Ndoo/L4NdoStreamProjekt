528e70bf10f5b663bee1812bed530f9c6b32c173
Thomas Frischholz <Thomas.Frischholz@igz.com>
2022-02-14 14:27:05 +0100

    Added FFMPEG Test project

---
+++ VideoStreamer/VideoStreamerCli/Program.cs
@@ -0,0 +1,70 @@
+﻿using FFmpeg.AutoGen;
+using SkiaSharp;
+using VideoStreamer.Cli.Util;
+using Size = System.Drawing.Size;
+
+namespace VideoStreamer.Cli;
+
+internal unsafe class Program
+{
+    private static string _outputFile = "output.mp4";
+    private static string _inputDirectory = "frames";
+    private static Size _imageSize = new Size(800, 800);
+
+    static int Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            string executable = Environment.GetCommandLineArgs()[0];
+            Console.WriteLine($"Usage: {executable} <frames directory> <output file>");
+            Console.WriteLine($"Using default frames directory <{_inputDirectory}> and output file <{_outputFile}>");
+        }
+        else
+        {
+            _inputDirectory = args[0];
+            _outputFile = args[1];
+        }
+
+        if (File.Exists(_outputFile)) { File.Delete(_outputFile); }
+        using Stream stream = File.Create(_outputFile);
+
+        using VideoStreamEncoder encoder = new VideoStreamEncoder(stream, 10, _imageSize);
+        using VideoFrameConverter frameConverter = new VideoFrameConverter(
+            new Size(4096, 4096),
+            AVPixelFormat.AV_PIX_FMT_RGB0,
+            _imageSize,
+            AVPixelFormat.AV_PIX_FMT_YUV420P);
+
+        IEnumerable<string> frameFiles = Directory.GetFiles(_inputDirectory);
+        foreach (string file in frameFiles)
+        {
+            using SKBitmap image = SKBitmap.Decode(file);
+            if(image == null) { continue; }
+
+            IntPtr pixelsAddr = image.GetPixels();
+            byte* ptr = (byte*)pixelsAddr.ToPointer();
+            byte_ptrArray8 data = new();
+            data[0] = ptr;
+            int_array8 linesize = new();
+            linesize[0] = image.Width * 4;
+
+            AVFrame rgbFrame = new AVFrame
+            {
+                data = data,
+                format = (int)AVPixelFormat.AV_PIX_FMT_RGB0,
+                linesize = linesize,
+                width = image.Width,
+                height = image.Height
+            };
+
+            AVFrame frame = frameConverter.Convert(rgbFrame);
+            encoder.Encode(frame);
+
+            Console.WriteLine($"Encoded '{file}'");
+        }
+
+        stream.Flush();
+        stream.Close();
+        return 0;
+    }
+}
---
+++ VideoStreamer/VideoStreamerCli/Util/VideoStreamEncoder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using FFmpeg.AutoGen;
+
+namespace VideoStreamer.Cli.Util
+{
+    public sealed unsafe class VideoStreamEncoder : IDisposable
+    {
+        private readonly AVCodec* _pCodec;
+        private readonly AVCodecContext* _pCodecContext;
+        private readonly Stream _stream;
+
+        public VideoStreamEncoder(Stream stream, int fps, Size frameSize)
+        {
+            _stream = stream;
+
+            var codecId = AVCodecID.AV_CODEC_ID_H264;
+            _pCodec = ffmpeg.avcodec_find_encoder(codecId);
+            if (_pCodec == null) throw new InvalidOperationException("Codec not found.");
+
+            _pCodecContext = ffmpeg.avcodec_alloc_context3(_pCodec);
+            _pCodecContext->width = frameSize.Width;
+            _pCodecContext->height = frameSize.Height;
+            _pCodecContext->time_base = new AVRational { num = 1, den = fps };
+            _pCodecContext->pix_fmt = AVPixelFormat.AV_PIX_FMT_YUV420P;
+            ffmpeg.av_opt_set(_pCodecContext->priv_data, "preset", "veryfast", 0);
+
+            ffmpeg.avcodec_open2(_pCodecContext, _pCodec, null).ThrowExceptionIfError();
+        }
+
+        public void Dispose()
+        {
+            ffmpeg.avcodec_close(_pCodecContext);
+            ffmpeg.av_free(_pCodecContext);
+        }
+
+        public void Encode(AVFrame frame)
+        {
+            var pPacket = ffmpeg.av_packet_alloc();
+
+            try
+            {
+                int error;
+
+                do
+                {
+                    ffmpeg.avcodec_send_frame(_pCodecContext, &frame).ThrowExceptionIfError();
+                    ffmpeg.av_packet_unref(pPacket);
+                    error = ffmpeg.avcodec_receive_packet(_pCodecContext, pPacket);
+                } while (error == ffmpeg.AVERROR(ffmpeg.EAGAIN));
+
+                error.ThrowExceptionIfError();
+
+                using var packetStream = new UnmanagedMemoryStream(pPacket->data, pPacket->size);
+                packetStream.CopyTo(_stream);
+            }
+            finally
+            {
+                ffmpeg.av_packet_free(&pPacket);
+            }
+        }
+    }
+}
